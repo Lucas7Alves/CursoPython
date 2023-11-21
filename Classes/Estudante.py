90f65b2538190c7246ce72cb0a89ae51ca605307
Lucas Alves <lucassalvess0909@gmail.com>
2023-11-20 21:17:25 -0300

    Classe em python
    praticando classes

---
+++ Classes/Estudante.py
@@ -0,0 +1,9 @@
+class Estudante_Cad:
+
+# esse init é inicializado toda vez que instanciar uma classe
+    def __init__(self, nome, idade, turma):
+
+        #self é tipo o this em Java
+        self.nome = nome
+        self.idade = idade
+        self.turma = turma
---
+++ Classes/Matricula.py
@@ -0,0 +1,7 @@
+from Estudante import Estudante_Cad
+
+aluno = Estudante_Cad("Jonas", 16, "1º-DS")
+aluno2 = Estudante_Cad("Emilly", 17, "3º-DS")
+
+print(f"Aluno(a): {aluno.nome}, Idade: {aluno.idade}, Turma: {aluno.turma}")
+print(f"Aluno(a): {aluno2.nome}, Idade: {aluno2.idade}, Turma: {aluno2.turma}")
